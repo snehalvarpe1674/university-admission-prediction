69d0e10d82b5308d4b6312a860ca86589335f1ff
snehalvarpe1674 <snehalvarpe1674@gmail.com>
2025-06-05 23:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,8 @@ def predict():
              # Render the HTML template with the prediction result
     return render_template('index.html', result=result)
 
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
